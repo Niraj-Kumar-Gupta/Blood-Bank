87c44306b58907e41536bc79881788be1278f988
Roshani Mandal <nirajgupta4606@gmail.com>
2023-10-07 00:23:15 +0530

    addition functionality

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin, AdminIndexView
 from flask_admin.contrib.sqla import ModelView
 from datetime import datetime
-
+from flask import jsonify
+from sqlalchemy import not_
  
 # instance of flask application
 app = Flask(__name__)
@@ -64,6 +65,8 @@ class blood_donate(db.Model):
         number = db.Column(db.Integer(),nullable=False)
         location = db.Column(db.String(50), nullable=False)
         date_created = db.Column(db.DateTime, default=datetime.now)
+        flag = db.Column(db.Integer,default="0")
+        address_details = db.Column(db.Integer, nullable=False)
 
 
         def get_id(self):
@@ -79,6 +82,8 @@ class blood_request(db.Model):
     number = db.Column(db.Integer(),nullable=False)
     location = db.Column(db.String(50), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.now)
+    flag = db.Column(db.Integer,default="0")
+    address_details = db.Column(db.Integer, nullable=False)
 
     def get_id(self):
          return self.sno   
@@ -264,7 +269,10 @@ def request_b():
     if user:
         req = blood_request.query.filter_by( first_name=user.first_name,email=user.email ).first()
         if req:
-            return render_template("request_blood.html",exist=True,accept=False,usr=user)
+            flag = req.flag
+            det=req.address_details
+            details=blood_donate.query.filter_by(sno=det).first()
+            return render_template("request_blood.html",exist=True,accept=flag,usr=user,details=details)
         else:
             return render_template("request_blood.html",exist=False,usr=user)
     else:
@@ -284,8 +292,9 @@ def blood_request1():
                 email = request.form['email']
                 number = request.form['number']
                 location = request.form['location']
+                address_details=0
                 req = blood_request(first_name=first_name,last_name=last_name,blood=blood,
-                        email=email,number=number,location=location)
+                        email=email,number=number,location=location,address_details=address_details)
                 db.session.add(req)  # adding user if not exists
                 db.session.commit()
                 req = blood_request.query.filter_by( first_name=first_name,email=email).first()
@@ -302,9 +311,12 @@ def donate():
    if user:
         req = blood_donate.query.filter_by( first_name=user.first_name,email=user.email ).first()
         if req:
-            return render_template("donate.html",exist=True,accept=False,usr=user)
+            flag = req.flag
+            det=req.address_details
+            details=blood_request.query.filter_by(sno=det).first()
+            return render_template("donate.html",exist=True,accept=flag,usr=user,details=details)
         else:
-            return render_template("donate.html",exist=True,accept=True,usr=user)
+            return render_template("donate.html",exist=False,usr=user)
    else:
       return render_template("login.html",message="login required !!")
 
@@ -321,9 +333,10 @@ def donate_blood():
                 email = request.form['email']
                 number = request.form['number']
                 location = request.form['location']
+                address_details=0
 
                 donate = blood_donate(first_name=first_name,last_name=last_name,blood=blood,dob=dob,
-                        email=email,number=number,location=location)
+                        email=email,number=number,location=location,address_details=address_details)
                 db.session.add(donate)  # adding user if not exists
                 db.session.commit()
                 req = blood_donate.query.filter_by( first_name=first_name,email=email).first()
@@ -331,8 +344,113 @@ def donate_blood():
                  return redirect("/donate")
         
  
-   
+@login_required
+@app.route('/notification')
+def notify():
+    return render_template("notification.html",users=blood_donate)
+  
 
+@app.route('/adminl')
+def admin_login():
+      # Fetch all blood donation records from the database
+    return render_template("admin.html")
+
+@app.route('/display_donar')
+def display_donar():
+    blood_donate_records = blood_donate.query.filter(not_(blood_donate.flag == 2)).all()
+    return render_template("display_donar.html", blood_donate_records=blood_donate_records)
+
+@app.route('/display_request')
+def display_request():
+    blood_request_records = blood_request.query.filter(not_(blood_request.flag == 2)).all()
+    return render_template("display_request.html", blood_request_records=blood_request_records)
+
+@app.route('/display_donar/<string:blood>', methods=['GET'])
+def display_donar1(blood):
+     request_list = blood_request.query.filter_by(blood=blood,flag=1).all()
+     # Convert the request_list to a list of dictionaries
+     request_list_data = [{'sno':request.sno,'first_name': request.first_name,'last_name': request.last_name, 'blood': request.blood, 'location': request.location,'email':request.email,'number':request.number} for request in request_list]
+     return jsonify(request_list_data)
+
+@app.route('/display_request/<string:blood>', methods=['GET'])
+def display_request1(blood):
+     request_list = blood_donate.query.filter_by(blood=blood,flag=1).all()
+     # Convert the request_list to a list of dictionaries
+     request_list_data = [{'sno':request.sno,'first_name': request.first_name,'last_name': request.last_name, 'blood': request.blood, 'location': request.location,'email':request.email,'number':request.number} for request in request_list]
+     return jsonify(request_list_data)
+    
+### display_donar start ##
+@app.route('/delete_request/<int:record_id>', methods=['POST'])
+def delete_record(record_id):
+    record = blood_donate.query.get(record_id)
+    if record:
+        db.session.delete(record)
+        db.session.commit()
+    return redirect('/display_donar')
+
+
+@app.route('/accept_request/<int:record_id>', methods=['POST'])
+def accept_request(record_id):
+    record = blood_donate.query.get(record_id)
+    if record:
+        record.flag = True  # Assuming 'flag' is a boolean field indicating acceptance
+        db.session.commit()
+    return redirect('/display_donar')
+
+@app.route('/details_send/<int:donar_id>', methods=['GET','POST'])
+def details_send(donar_id):
+    if request.method == "POST": 
+       donar_id = donar_id
+       select_request_1 = request.form['select_request_1']
+       if donar_id and select_request_1:
+         donar_database = blood_donate.query.get(donar_id)
+         receiver_database= blood_request.query.get(select_request_1)
+         donar_database.address_details=select_request_1
+         receiver_database.address_details=donar_id
+         donar_database.flag=2
+         receiver_database.flag=2
+         db.session.commit()
+       print("Donor ID:", donar_id)
+       print("Selected Request:", select_request_1)
+    return redirect('/display_donar')
+       
+    
+
+
+# request start ##
+@app.route('/delete_request_receiver/<int:record_id>', methods=['POST'])
+def delete_record_request(record_id):
+    record = blood_request.query.get(record_id)
+    if record:
+        db.session.delete(record)
+        db.session.commit()
+    return redirect('/display_request')
+
+
+@app.route('/accept_request_receiver/<int:record_id>', methods=['POST'])
+def accept_request_receiver(record_id):
+    record = blood_request.query.get(record_id)
+    if record:
+        record.flag = True  # Assuming 'flag' is a boolean field indicating acceptance
+        db.session.commit()
+    return redirect('/display_request')
+
+@app.route('/details_send1/<int:request_id>', methods=['GET','POST'])
+def details_send1(request_id):
+    if request.method == "POST": 
+       request_id = request_id
+       select_request_1 = request.form['select_request_1']
+       if request_id and select_request_1:
+         donar_database = blood_donate.query.get(select_request_1)
+         receiver_database= blood_request.query.get(request_id)
+         donar_database.address_details=request_id
+         receiver_database.address_details=select_request_1
+         donar_database.flag=2
+         receiver_database.flag=2
+         db.session.commit()
+       print("Request ID:", request_id)
+       print("Donar Request:", select_request_1)
+    return redirect('/display_request')
 
 if __name__ == '__main__': 
-   app.run(debug=True,host="0.0.0.0",port=8000)
+   app.run(debug=False,host="0.0.0.0",port="8000")
